1f728ec79f9577ebbe785eb6629bf9ca46d1bfb9
box-automation1 <n.potest12343727@gmail.com>
2021-09-27 11:26:43 +0530

    Update dump.py

--- bot/modules/dump.py
+++ bot/modules/dump.py
@@ -136,7 +136,7 @@ def dump(update: Update, context: CallbackContext):
     message = update.effective_message
     cmd = message.text.split(' ', 1)
     CHAT_ID=message.chat_id
-    uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
+    uname = f'<a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>'
     print(CHAT_ID)
     if len(cmd) == 1:
         message.reply_text('Please Provide a Direct Link to an Android Firmware')
